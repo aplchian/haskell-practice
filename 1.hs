293b2fff8a7f13f41d4429880397079548de59fe
aplchian <alex.boquis@gmail.com>
2017-07-30 22:34:40 -0400

    added right triangle example

--- 1.hs
+++ 1.hs
@@ -15,3 +15,5 @@ length' xs = [ succ x | x <- xs ]
 removeNonUpperCase xs = [ x | x <- xs, x `elem` ['A'..'Z'] ]
 
 addIndex x = zip [1..] x 
+
+-- let rightTriangles' = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == 24]
