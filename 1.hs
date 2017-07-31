537da9ba0a2cc40c0f8bb6fc4c82c5c4851dcb24
aplchian <alex.boquis@gmail.com>
2017-07-30 22:29:49 -0400

    used zip

--- 1.hs
+++ 1.hs
@@ -7,3 +7,11 @@ myButLast' x = reverse x !! 1
 myButLast'' =  head . tail . reverse
 
 elementAt x y = x !! pred y
+
+-- sum xs = foldl (\acc x -> acc + x) 0 xs  
+
+length' xs = [ succ x | x <- xs ]
+
+removeNonUpperCase xs = [ x | x <- xs, x `elem` ['A'..'Z'] ]
+
+addIndex x = zip [1..] x 
