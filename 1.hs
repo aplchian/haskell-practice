ffffbea384275a2e85026f8f3614e8662df6476b
aplchian <alex.boquis@gmail.com>
2017-09-02 19:03:00 -0400

    foldR map

--- 1.hs
+++ 1.hs
@@ -104,5 +104,7 @@ filter' f (x:xs)
     | otherwise = filter' f xs
 
 sum' :: (Num a) => [a] -> a
-sum' xs = foldl (\acc x -> acc + x) 0 xs 
+sum' xs = foldl (+) 0 xs 
 
+map'' :: (a -> b) -> [a] -> [b]
+map'' f xs = foldr (\x acc -> f x : acc ) [] xs
