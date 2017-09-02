46d3586473307d457dd2d70457d6f6996cbb5a90
aplchian <alex.boquis@gmail.com>
2017-09-02 19:10:37 -0400

    added elem foldr

--- 1.hs
+++ 1.hs
@@ -108,3 +108,6 @@ sum' xs = foldl (+) 0 xs
 
 map'' :: (a -> b) -> [a] -> [b]
 map'' f xs = foldr (\x acc -> f x : acc ) [] xs
+
+elem'' :: (Eq a) => a -> [a] -> Bool
+elem'' y ys = foldr (\x acc -> if x == y then True else acc ) False ys
