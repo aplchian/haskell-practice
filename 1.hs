8ad37d39c9d2e5274ef85ca1f5bad0e29b647423
aplchian <alex.boquis@gmail.com>
2017-09-04 19:57:19 -0400

    maximum with foldlr1

--- 1.hs
+++ 1.hs
@@ -111,3 +111,8 @@ map'' f xs = foldr (\x acc -> f x : acc ) [] xs
 
 elem'' :: (Eq a) => a -> [a] -> Bool
 elem'' y ys = foldr (\x acc -> if x == y then True else acc ) False ys
+
+maximum'' :: (Ord a) => [a] -> a
+maximum'' = foldl1 max
+
+
